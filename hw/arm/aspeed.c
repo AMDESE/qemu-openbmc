d9dd87c26632b21ee40a6a7666f702b9e9308ced
Jayanth Othayoth <Jayanth.Othayoth@amd.com>
2024-07-17 07:16:33 -0500

    hw/arm/aspeed: Adding new AMD CRB Marley
    Patch to support Marley CRB in QEMU environment.
    
    Test:
      BUILDDIR= <xx>/openbmc/build/sp7
      IMGDIR=${BUILDDIR}/tmp/deploy/images/sp7
      DEPLOYDIR=${BUILDDIR}/tmp/work/sp7-openbmc-linux/u-boot-aspeed-sdk/v2023.10+git/build
      UBOOT_SIZE=$(stat --format=%s -L ${DEPLOYDIR}/u-boot-nodtb.bin)
      UBOOT_DTB_ADDR=$((0x400000000 + ${UBOOT_SIZE}))
      ./build/qemu-system-aarch64 -M marley-bmc -nographic \
      -device loader,addr=0x400000000,file=${DEPLOYDIR}/u-boot-nodtb.bin,force-raw=on \
      -device loader,addr=${UBOOT_DTB_ADDR},file=${DEPLOYDIR}/u-boot.dtb,force-raw=on \
      -device loader,addr=0x430000000,file=${IMGDIR}/bl31.bin,force-raw=on \
      -device loader,addr=0x430080000,file=${IMGDIR}/optee/tee-raw.bin,force-raw=on \
      -device loader,addr=0x430000000,cpu-num=0 \
      -device loader,addr=0x430000000,cpu-num=1 \
      -device loader,addr=0x430000000,cpu-num=2 \
      -device loader,addr=0x430000000,cpu-num=3 \
      -smp 4 \
      -drive file=${IMGDIR}/image-bmc,format=raw,if=mtd \
      -serial stdio \
      -snapshot \
      -monitor telnet:127.0.0.1:1234,server,nowait \
    
    Signed-off-by: Jayanth Othayoth <Jayanth.Othayoth@amd.com>

--- hw/arm/aspeed.c
+++ hw/arm/aspeed.c
@@ -185,6 +185,10 @@ struct AspeedMachineState {
 #define AST2700_EVB_HW_STRAP1 0x00000800
 /* SCUIO HW Strap1 */
 #define AST2700_EVB_HW_STRAP2 0x00000700
+
+/* Malta hardware value */
+#define MALTA_BMC_HW_STRAP1 AST2700_EVB_HW_STRAP1
+#define MALTA_BMC_HW_STRAP2 AST2700_EVB_HW_STRAP2
 #endif
 
 /* Rainier hardware value: (QEMU prototype) */
@@ -1920,6 +1924,26 @@ static void aspeed_machine_ast2700a1_evb_class_init(ObjectClass *oc,
     mc->default_ram_size = 1 * GiB;
     aspeed_machine_class_init_cpus_defaults(mc);
 }
+
+static void aspeed_machine_marley_class_init(ObjectClass *oc, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+    AspeedMachineClass *amc = ASPEED_MACHINE_CLASS(oc);
+
+    mc->desc = "AMD Marley BMC (Cortex-A35)";
+    amc->soc_name  = "ast2700-a1";
+    amc->hw_strap1 = MALTA_BMC_HW_STRAP1;
+    amc->hw_strap2 = MALTA_BMC_HW_STRAP2;
+    amc->fmc_model = "w25q01jvq";
+    amc->spi_model = "w25q512jv";
+    amc->num_cs    = 2;
+    amc->macs_mask = ASPEED_MAC0_ON | ASPEED_MAC1_ON | ASPEED_MAC2_ON;
+    amc->uart_default = ASPEED_DEV_UART12;
+    amc->i2c_init  = marley_bmc_i2c_init;
+    mc->default_ram_size = 2 * GiB;
+    aspeed_machine_class_init_cpus_defaults(mc);
+}
+
 #endif
 
 static void aspeed_machine_qcom_dc_scm_v1_class_init(ObjectClass *oc,
@@ -2056,10 +2080,14 @@ static const TypeInfo aspeed_machine_types[] = {
         .name          = MACHINE_TYPE_NAME("ast2700a0-evb"),
         .parent        = TYPE_ASPEED_MACHINE,
         .class_init    = aspeed_machine_ast2700a0_evb_class_init,
-        }, {
+    }, {
         .name          = MACHINE_TYPE_NAME("ast2700a1-evb"),
         .parent        = TYPE_ASPEED_MACHINE,
         .class_init    = aspeed_machine_ast2700a1_evb_class_init,
+    }, {
+        .name          = MACHINE_TYPE_NAME("marley-bmc"),
+        .parent        = TYPE_ASPEED_MACHINE,
+        .class_init    = aspeed_machine_marley_class_init,
 #endif
     }, {
         .name          = TYPE_ASPEED_MACHINE,
